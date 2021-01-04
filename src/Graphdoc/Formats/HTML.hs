64a98bc91ee6eecfc34d11616f1ec0592dbea4d2
Tassos Manganaris <tassos.manganaris@gmail.com>
2021-01-03 20:28:29 -0500

    Added HTML format.

---
+++ src/Graphdoc/Formats/HTML.hs
@@ -0,0 +1,16 @@
+module Graphdoc.Formats.HTML
+  ( relations ) where
+
+import Graphdoc.Definition
+
+relations :: [DocRelation]
+relations = undefined
+
+getNext :: DocRelation
+getNext = undefined
+
+getUp :: DocRelation
+getUp = undefined
+
+getPrev :: DocRelation
+getPrev = undefined
