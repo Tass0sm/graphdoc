cc691f2eafd3f169f2162ea9654c6062ac42f886
Tassos Manganaris <tassos.manganaris@gmail.com>
2022-02-23 23:08:36 -0500

    Clean guix file

--- guix.scm
+++ guix.scm
@@ -16,48 +16,22 @@
 (define ghc-graphdoc
   (package
    (name "ghc-graphdoc")
-   (version "0.19.5")
+   (version "0.0.1")
    (source
     (local-file %source-dir
                 #:recursive? #t
                 #:select? (git-predicate %source-dir)))
-   ;; (origin
-   ;;  (method url-fetch)
-   ;;  (uri "file:///home/tassos/software/haskell/graphdoc")
-   ;;  (sha256
-   ;;   (base32
-   ;;    "1z4x1cc7m720n3x7k58547pl4gxk6xbhvls28adirdsc04phkqym")))
    (build-system haskell-build-system)
+;;    (native-inputs
+;;     (list cabal-install))
    (inputs
     (list ghc-pandoc
           ghc-pandoc-types))
-   (home-page
-    "")
-   (synopsis
-    "")
-   (description
-    "")
-   (license license:expat)))
+   (home-page "")
+   (synopsis "")
+   (description "")
+   (license license:gpl3+)))
 
-ghc-graphdoc
-
-;; (define ghc-graphdoc
-;;   (package
-;;    (name "ghc-graphdoc")
-;;    (version "0.0.1")
-;;    (source (local-file "."))
-;;    (build-system haskell-build-system)
-;;    (native-inputs
-;;     (list cabal-install))
-;;    ;; (arguments
-;;    ;;  `(#:phases
-;;    ;;    (modify-phases %standard-phases
-;;    ;;                   (delete 'unpack))))
-;;    (home-page "")
-;;    (synopsis "")
-;;    (description "")
-;;    (license license:gpl3+)))
-
-;; (package
-;;  (inherit ghc-graphdoc)
-;;  (name "graphdoc"))
+(package
+ (inherit ghc-graphdoc)
+ (name "graphdoc"))
